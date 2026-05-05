b0e0ea39a5bfd5bc7207897d64d9808f793ace25
hjonatha <jonathan.hasard@ynov.com>
2026-05-05 12:28:45 +0200

    [FIX] fixing app.js

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
+const path = __dirname + '/views/';
 const port = 8080;
 
-router.use(function (req, res, next) {
+router.use(function (req,res,next) {
     console.log('/' + req.method);
     next();
 });
 
-router.get('/', function(req, res) {
-    res.send('Forum app is running!');
+router.get('/', function(req,res){
+    res.sendFile(path + 'index.html');
 });
 
+router.get('/sharks', function(req,res){
+    res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
 app.use('/', router);
 
 app.listen(port, function () {
     console.log('Example app listening on port 8080!')
-});
+})
