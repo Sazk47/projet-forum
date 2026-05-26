63852022969bdaea21a3b3e103b4e545778ebbae
hjonatha <jonathan.hasard@ynov.com>
2026-05-26 09:58:32 +0200

    [FIX] fixing app.js, express framework is not authorized

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-
-const path = __dirname + '/views/';
+const http = require("http");
+const host = 'localhost';
 const port = 8080;
 
-router.use(function (req,res,next) {
-    console.log('/' + req.method);
-    next();
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("Server is running!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
 });
-
-router.get('/', function(req,res){
-    res.sendFile(path + 'index.html');
-});
-
-router.get('/sharks', function(req,res){
-    res.sendFile(path + 'sharks.html');
-});
-
-app.use(express.static(path));
-app.use('/', router);
-
-app.listen(port, function () {
-    console.log('Example app listening on port 8080!')
-})
