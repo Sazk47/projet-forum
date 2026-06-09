266187af72d9d945e988843795f6c8a286c70bdd
maeva87 <maeva.neveu@ynov.com>
2026-06-09 09:18:17 +0200

    [ADD] add login

--- app.js
+++ app.js
@@ -57,3 +57,16 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`Serveur lancé sur le port ${PORT}`);
 });
+const http = require("http");
+const host = 'localhost';
+const port = 8080;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Server is running!");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
